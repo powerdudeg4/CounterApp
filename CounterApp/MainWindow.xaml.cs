2d63463d861e2729297a2a35b62f55726dc3f5cb
stalker0912 <36169761+stalker0912@users.noreply.github.com>
2020-05-02 20:51:55 +0200

    v1

--- CounterApp/MainWindow.xaml.cs
+++ CounterApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace CounterApp
 {
@@ -24,5 +11,35 @@ namespace CounterApp
         {
             InitializeComponent();
         }
+        [System.Windows.Localizability(System.Windows.LocalizationCategory.Text)]
+        public string Text { get; set; }
+    
+        /// <summary>
+        /// This is a part that does the actual hard dwork
+        /// </summary>
+        public int num = 0;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            num++;
+            Number.Text = num.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            num--;
+            Number.Text = num.ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Window1 win1 = new Window1();
+            win1.Show();
+        }
     }
+
 }
---
+++ CounterApp/a.cs
@@ -0,0 +1,6 @@
+﻿namespace CounterApp
+{
+    internal class a
+    {
+    }
+}
